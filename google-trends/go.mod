e13019fb8c63a36fba399d72338ddb448217bfec
christianotieno <christian.otieno@saltpay.co>
2023-06-10 00:12:35 +0100

    feat: add google trends app

---
+++ google-trends/go.mod
@@ -0,0 +1,3 @@
+module github.com/christianotieno/google-trends
+
+go 1.19
---
+++ google-trends/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type RSS struct {
+	XMLName xml.Name `xml:"rss"`
+	Channel *Channel `xml:"channel"`
+}
+
+type Channel struct {
+	Title    string `xml:"title"`
+	ItemList []Item `xml:"item"`
+}
+
+type Item struct {
+	Title     string `xml:"title"`
+	Link      string `xml:"link"`
+	Traffic   string `xml:"ht:approx_traffic"`
+	NewsItems []News `xml:"ht:news_item"`
+}
+
+type News struct {
+	Headline     string `xml:"news_item_title"`
+	HeadlineLink string `xml:"news_item_url"`
+}
+
+func main() {
+	var r RSS
+	data := readGoogleTrends()
+
+	err := xml.Unmarshal(data, &r)
+
+	if err != nil {
+		fmt.Println("error", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("\n Below are the top 20 Google Trends for the day !")
+	fmt.Println("-------------------------------------------------")
+	for i, item := range r.Channel.ItemList {
+		rank := i + 1
+		fmt.Println("Rank: ", rank)
+		fmt.Println("Search Term: ", item.Title)
+		fmt.Println("Link to the trend: ", item.Link)
+		fmt.Println("News: ")
+		for _, news := range item.NewsItems {
+			fmt.Println("Headline: ", news.Headline)
+			fmt.Println("Headline Link: ", news.HeadlineLink)
+		}
+		fmt.Println("-------------------------------------------------")
+	}
+}
+
+func getGoogleTrends() *http.Response {
+	resp, err := http.Get("https://trends.google.com/trends/trendingsearches/daily/rss?geo=PT")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return resp
+}
+
+func readGoogleTrends() []byte {
+	resp := getGoogleTrends()
+
+	data, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return data
+}
